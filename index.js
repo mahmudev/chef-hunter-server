a5ebf19f4e780a9882076c428ad8edaa6d19a9db
Nazrul Islam Mahmud <69884792+mahmudev@users.noreply.github.com>
2023-05-02 23:20:55 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChefs = chefs.find(n => n.id === id) || {};
+    res.send(selectedChefs)
+})
+
+app.listen(port, () => {
+    console.log(`Server API is running on port: ${port}`)
+})
